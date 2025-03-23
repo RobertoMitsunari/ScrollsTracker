ec250923b12b3301755be0853465f5b157da40a4
RobertoMitsunari <roberto.mitsunari06@gmail.com>
2025-03-23 03:23:53 -0300

    Revert "Revert "Merge Master""
    This reverts commit ccf09bc478ad373fa3d1acc65024ecc7cb9872a8.

---
+++ ScrollsTracker-WebApp/src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<script setup>
+import TitleCard from '../components/TitleCard.vue'
+</script>
+
+<template>
+    <div>
+        <div class="container text-body-secondary">
+            <div class="row mt-4">
+                <div class="col-sm-12 col-md-6">
+                    <TitleCard />
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style>
+    
+</style>
