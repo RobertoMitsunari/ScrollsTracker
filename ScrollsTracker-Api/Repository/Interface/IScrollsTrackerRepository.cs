1f5f0eed2b858fece426208c441b11d1874b645d
RobertoMitsunari <roberto.mitsunari06@gmail.com>
2025-03-26 01:23:16 -0300

    FIX: Correcao repository

---
+++ ScrollsTracker-Api/Repository/Interface/IScrollsTrackerRepository.cs
@@ -0,0 +1,10 @@
+﻿using ScrollsTracker.Api.Model;
+
+namespace ScrollsTracker.Api.Repository.Interface
+{
+    public interface IScrollsTrackerRepository
+    {
+        IList<Obra> ObterObras();
+        Task<int> CadastrarObrasAsync(Obra obra);
+    }
+}
---
+++ ScrollsTracker-Api/Repository/ScrollsTrackerRepository.cs
@@ -0,0 +1,27 @@
+﻿using ScrollsTracker.Api.Model;
+using ScrollsTracker.Api.Repository.Context;
+using ScrollsTracker.Api.Repository.Interface;
+
+namespace ScrollsTracker.Api.Repository
+{
+    public class ScrollsTrackerRepository : IScrollsTrackerRepository
+    {
+        private readonly AppDbContext _context;
+
+        public ScrollsTrackerRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> CadastrarObrasAsync(Obra obra)
+        {
+            _context.Obras.Add(obra);
+            return await _context.SaveChangesAsync();
+        }
+
+        public IList<Obra> ObterObras()
+        {
+            return _context.Obras.ToList();
+        }
+    }
+}
